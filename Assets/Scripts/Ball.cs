4d71dff321970919aa25caf9040726f7e66b00dd
pekkipo <pekkipodev@gmail.com>
2017-02-02 22:01:52 +0300

    Final State

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	public Vector3 launchVeclocity;
+	public bool inPlay = false; // this is in order to forbid influencing the ball after its launch
+
+	private Vector3 ballStartPos;
+	private Rigidbody rigidBody;
+	private AudioSource audioSource;
+	
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>(); // Do this a lot in Unity 5
+		rigidBody.useGravity = false;
+		ballStartPos = transform.position;
+	}
+	
+	public void Launch (Vector3 velocity) // this function is called in BallDragLaunch script and
+        //velocity is determined by the direction and strength of a tap or mouse click
+	{
+		inPlay = true;
+
+		rigidBody.useGravity = true;
+		rigidBody.velocity = velocity;
+
+		audioSource = GetComponent<AudioSource> ();
+		audioSource.Play ();
+	}
+
+	public void Reset () {
+		inPlay = false;
+		transform.position = ballStartPos;
+		transform.rotation = Quaternion.identity;
+		rigidBody.velocity = Vector3.zero;
+		rigidBody.angularVelocity = Vector3.zero;
+		rigidBody.useGravity = false;
+	}
+}
---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pin : MonoBehaviour {
+
+	public float standingThreshold = 3f; // remember if it is changed in inspector - it will
+    //be overrided. Set value for all pins - select them all and type a new value in an Inspector
+	public float distToRaise = 40f;
+
+	private Rigidbody rigidBody;
+	
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	
+	public bool IsStanding () {
+		Vector3 rotationInEuler = transform.rotation.eulerAngles; // Nice method to determine the
+        //rotation values of an object
+
+		float tiltInX = Mathf.Abs(270 - rotationInEuler.x); // cause we need abs value regardless of the
+        // sign like 8 degrees or -352 makes no difference
+		float tiltInZ = Mathf.Abs(rotationInEuler.z);
+        // x and z are enough
+        // 270 we need after we changed the pins rotation parameters
+
+		if (tiltInX < standingThreshold && tiltInZ < standingThreshold) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void RaiseIfStanding () {
+		if (IsStanding ()) {
+			rigidBody.useGravity = false;
+			transform.Translate (new Vector3 (0, distToRaise, 0), Space.World);
+			transform.rotation = Quaternion.Euler (270f, 0, 0);
+		}
+	}
+
+	public void Lower () {
+		transform.Translate (new Vector3 (0, -distToRaise, 0), Space.World);
+		rigidBody.useGravity = true;
+	}
+
+}
